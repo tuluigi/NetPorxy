7a4d1f90627aaec00f734035ebbf2cf37c5823ca
tujinguo <tujinguo@corp.netease.com>
2018-01-04 17:56:44 +0800

    fix bug

--- NetAgency/ViewController.m
+++ NetAgency/ViewController.m
@@ -13,6 +13,10 @@
 @property (nonatomic ,strong) NSArray *dataSources;
 @property (nonatomic ,strong) UITableView *tableview;
 @property (nonatomic ,strong) PacketTunnelProvider *tunnelProvider;
+
+@property (nonatomic ,strong) NETunnelProviderManager *manager;
+
+@property (nonatomic ,strong) UIButton *button;
 @end
 
 @implementation ViewController
@@ -21,9 +25,11 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
+    [self createVpn];
+    
     UIButton *button =[UIButton buttonWithType:UIButtonTypeCustom];
     [button setTitle:@"Start" forState:UIControlStateNormal];
-    [button setTitle:@"Close" forState:UIControlStateSelected];
+//    [button setTitle:@"Close" forState:UIControlStateSelected];
     
     CGFloat width =100.0f;
     button.frame = CGRectMake(0, 0, width, width);
@@ -34,6 +40,8 @@
     [button addTarget:self action:@selector(onVpnAction:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:button];
     
+    self.button = button;
+    
     
 }
 
@@ -42,25 +50,20 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-- (void)onVpnAction:(UIButton *)sender{
-    if (sender.selected) {
-        [self stopVPN];
-    }else{
-        [self startVpn];
-    }
-    sender.selected = !sender.selected;
-}
 
-- (void)startVpn{
+- (void)createVpn{
     [NETunnelProviderManager loadAllFromPreferencesWithCompletionHandler:^(NSArray<NETunnelProviderManager *> * _Nullable managers, NSError * _Nullable error) {
         NETunnelProviderManager *manager = nil;
         if (managers.count==0) {
             //create manager
             manager = (NETunnelProviderManager *)[NETunnelProviderManager sharedManager];
-            NETunnelProviderProtocol *protocal = [[NETunnelProviderProtocol alloc]  init];
-            protocal.serverAddress = @"104.20.82.194";
-//            protocal.username = @"";
-            manager.protocolConfiguration = protocal;
+            NEVPNProtocolIKEv2 *p = [[NEVPNProtocolIKEv2 alloc] init];
+            p.username = @"";
+            p.serverAddress = @"";
+            p.passwordReference = nil;
+            p.localIdentifier = @"";
+            p.remoteIdentifier = @"";
+            manager.protocolConfiguration = p;
         }else{
             manager = managers.firstObject;
         }
@@ -69,25 +72,66 @@
         manager.onDemandEnabled = YES;
         manager.localizedDescription= @"MyVpn";
         
-       // manager.protocolConfiguration =[nevpo];
+        // manager.protocolConfiguration =[nevpo];
         [manager saveToPreferencesWithCompletionHandler:^(NSError * _Nullable error) {
             if (nil == error) {
-                
+               
             }
         }];
-        /*
-       NSError *aError;
-       BOOL isSucceed = [manager.connection startVPNTunnelWithOptions:nil andReturnError:&aError];
-        if (isSucceed&&nil==aError) {
-            
-        }else{
-            
-        }
-         */
+        self.manager = manager;
     }];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onVpnStateChange:) name:NEVPNStatusDidChangeNotification object:nil];
+}
+
+- (void)onVpnAction:(UIButton *)sender{
+    switch (self.manager.connection.status) {
+        case NEVPNStatusConnected:
+            [self stopVPN];
+            break;
+        case NEVPNStatusDisconnected:{
+            [self startVpn];
+        }break;
+        case NEVPNStatusInvalid:{
+            NSLog(@"配置失败");
+        }break;
+        default:
+            break;
+    }
+}
+- (void)onVpnStateChange:(NSNotification *)Notification{
+    NEVPNStatus state = self.manager.connection.status;
+    switch (state) {
+        case NEVPNStatusInvalid:
+             [self.button setTitle:@"Start" forState:UIControlStateNormal];
+            NSLog(@"无效连接");
+            break;
+        case NEVPNStatusDisconnected:
+            [self.button setTitle:@"Start" forState:UIControlStateNormal];
+            NSLog(@"未连接");
+            break;
+        case NEVPNStatusConnecting:
+            [self.button setTitle:@"Connecting" forState:UIControlStateNormal];
+            NSLog(@"正在连接");
+            break;
+        case NEVPNStatusConnected:
+            [self.button setTitle:@"Stop" forState:UIControlStateNormal];
+            NSLog(@"已连接");
+            break;
+        case NEVPNStatusDisconnecting:
+             [self.button setTitle:@"Disconnecting" forState:UIControlStateNormal];
+            NSLog(@"断开连接");
+            break;
+        default:
+            break;
+    }
+}
+
+- (void)startVpn{
+    [self.manager.connection startVPNTunnelWithOptions:nil andReturnError:nil];
 }
 - (void)stopVPN{
-    
+    [self.manager.connection stopVPNTunnel];
 }
 #pragma mark - Getter
 - (NSArray *)dataSources{
